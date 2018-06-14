9b37fb1b20d0d865d3b3fcde20f77e26522039a5
CODEheures <webmaster@codeheures.fr>
2018-06-15 00:53:41 +0200

    add inputs on index page

--- pages/_lang/index.vue
+++ pages/_lang/index.vue
@@ -5,25 +5,37 @@
         <img src="~/assets/images/logo.png" alt="Vuetify.js" class="mb-5" />
       </div>
       <v-card>
-        <v-card-title class="headline">Welcome to the Vuetify + Nuxt.js template</v-card-title>
+        <v-card-title class="headline">{{ $t('home.search.title') }}</v-card-title>
         <v-card-text>
-          <p>{{ $t('home.title') }}</p>
-          <!--<p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>-->
-          <!--<p>For more information on Vuetify, check out the <a href="https://vuetifyjs.com" target="_blank">documentation</a>.</p>-->
-          <!--<p>If you have questions, please join the official <a href="https://chat.vuetifyjs.com/" target="_blank" title="chat">discord</a>.</p>-->
-          <!--<p>Find a bug? Report it on the github <a href="https://github.com/vuetifyjs/vuetify/issues" target="_blank" title="contribute">issue board</a>.</p>-->
-          <!--<p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>-->
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
+          <p>{{ $t('home.search.helper') }}</p>
           <hr class="my-3">
-          <a href="https://nuxtjs.org/" target="_blank">Nuxt Documentation</a>
-          <br>
-          <a href="https://github.com/nuxt/nuxt.js" target="_blank">Nuxt GitHub</a>
+          <v-text-field id="place" name="place" :label="$t('home.search.place')" prepend-icon="place"/>
+          <v-slider v-model="media" prepend-icon="360" max="250" ticks step="10" thumb-label validate-on-blur id="slide-distance"></v-slider>
+          <v-select
+            v-model="searchs"
+            :label="$t('home.search.searchs.label')"
+            chips
+            tags
+            solo
+            prepend-icon="format_list_bulleted"
+            clearable
+          >
+            <template slot="selection" slot-scope="data">
+              <v-chip
+                :selected="data.selected"
+                close
+                @input="remove(data.item)"
+                color="primary"
+                outline
+              >
+                <strong>{{ data.item }}</strong>
+              </v-chip>
+            </template>
+          </v-select>
         </v-card-text>
         <v-card-actions>
           <v-spacer></v-spacer>
-          <v-btn color="primary" flat nuxt to="/inspire">Continue</v-btn>
+          <v-btn color="primary" flat nuxt to="/inspire">{{ $t('home.search.button') }}</v-btn>
         </v-card-actions>
       </v-card>
     </v-flex>
@@ -37,6 +49,43 @@
       return {
         // apiDomain: Api.listRoutes()
       }
+    },
+    data () {
+      return {
+        media: 0,
+        searchs: []
+      }
+    },
+    mounted () {
+      if (process.client) {
+        document.querySelector('#slide-distance .input-group__input .slider').dataset.after = this.$t('home.search.distance')
+      }
+    },
+    methods: {
+      remove (item) {
+        this.searchs.splice(this.searchs.indexOf(item), 1)
+        this.searchs = [...this.searchs]
+      }
     }
   }
 </script>
+
+<style lang="stylus">
+  #slide-distance > .input-group__input > .slider
+    &:after
+      content attr(data-after)
+      color #757575
+      position absolute
+      right 0
+      bottom -8px
+  .slider__thumb--label__container
+    display block !important
+    transition none
+    & div.slider__thumb--label
+      width 36px
+      height 36px
+      left -18px
+      top -50px
+      & span:after
+        content "km"
+</style>
