d47bada9a609b911f47b96ffe0e46565de7ec958
CODEheures <webmaster@codeheures.fr>
2018-06-18 12:17:17 +0200

    change footer text and css

--- layouts/default.vue
+++ layouts/default.vue
@@ -47,11 +47,12 @@
       <v-container>
         <nuxt />
       </v-container>
-      <v-footer height="auto" >
+      <v-footer height="auto" style="width: 100%;" >
         <v-card
           flat
           tile
-          class="indigo lighten-1 white--text text-xs-center"
+          class="indigo lighten-1 white--text text-xs-center xs12"
+          width="100%"
         >
           <v-card-text>
             <v-btn
@@ -64,10 +65,10 @@
             </v-btn>
           </v-card-text>
           <v-card-text class="white--text pt-0">
-            Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet. Mauris cursus commodo interdum. Praesent ut risus eget metus luctus accumsan id ultrices nunc. Sed at orci sed massa consectetur dignissim a sit amet dui. Duis commodo vitae velit et faucibus. Morbi vehicula lacinia malesuada. Nulla placerat augue vel ipsum ultrices, cursus iaculis dui sollicitudin. Vestibulum eu ipsum vel diam elementum tempor vel ut orci. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+            {{ $t('footer.description') }}
           </v-card-text>
           <v-card-text class="white--text">
-            &copy;2018 — <strong>Vuetify</strong>
+            &copy;2018 — <strong>SelfJob</strong>
           </v-card-text>
         </v-card>
       </v-footer>
